4dd69f2edd910528efe7e85ec5fc5c021215bff2
Igor_Kukric <igorkukric@gmail.com>
2023-11-13 18:49:02 +0100

    add fiber, and drei

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import Loader from '../components/Loader'
+
+{/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
+  MAIN
+</div> */}
 
 const Home = () => {
   return (
     <section className='w-full h-screen relative'>
-      <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
-        MAIN
-      </div>
+        <Canvas 
+            className='w-full h-screen bg-transparent'
+            camera={{near: 0.1, far: 1000}}  
+          >
+          <Suspense fallback={<Loader />}>
+            <directionalLight />
+            <ambientLight />
+            <pointLight />
+            <spotLight />
+            <hemisphereLight />
+          </Suspense>
+        </Canvas>
     </section>
   )
 }
