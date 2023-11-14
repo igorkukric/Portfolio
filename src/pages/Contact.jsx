8e9d42378455d62ea561b7a8afb704c7bdeffb13
Igor_Kukric <igorkukric@gmail.com>
2023-11-14 15:07:28 +0100

    add emailjs and functionality to form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,42 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
+  const formRef = useRef(null)
   const [form, setForm] = useState({name: '', email: '', message: ''})
   const [isLoading, setIsLoading] = useState(false)
 
-  const handleChange = () => {}
+  const handleChange = (e) => {
+    setForm({...form, [e.target.name]: e.target.value})
+  }
+  
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setIsLoading(true)
+    // console.log(VITE_APP_EMAILJS_SERVICE_ID)
+    emailjs.send(
+      import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+      import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+      {
+        from_name: form.name,
+        to_name: "Igor",
+        from_email: form.email,
+        to_email: 'igorkukric@gmail.com',
+        message: form.message
+      },
+      import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+    ).then(()=> {
+      setIsLoading(false)
+      // TODO: show succes message
+      //TODO: HIde an alert
+      setForm({name: '', email: '', message: ''})
+    }).catch((error)=> {
+      setIsLoading(false)
+      console.log(error)
+      //TODO: Show error message
+    })
+  }
+
   const handleFocus = () => {}
   const handleBlur = () => {}
 
@@ -14,7 +46,10 @@ const Contact = () => {
       <div className='flex-1 min-w-[50%] flex flex-col'>
         <h1 className='head-text'>Get in Touch</h1>
 
-        <form className='w-full flex flex-col gap-7 mt-14'>
+        <form 
+        className='w-full flex flex-col gap-7 mt-14'
+        onSubmit={handleSubmit}
+        >
           <label className='text-black-500 font-semibold'>
             Name
             <input 
