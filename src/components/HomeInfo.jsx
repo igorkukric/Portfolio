a8e7c39bded6e8716ab901540404d44d5a271bda
Igor_Kukric <igorkukric@gmail.com>
2023-11-14 14:23:16 +0100

    add HomeInfo.jsx

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,35 +1,65 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
-const renderContent = {
-    1: (
-        <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue
-        py-4 px-8 text-white mx-5'>
-            Hi, I am <span className='font-semibold'>Igor</span>👋
-            <br />
-            Focusing on creating emotional experiences.
-        </h1>
-    ),
-    2: (
-        <h1>2</h1>
-    ),
-    3: (
-        <h1>3</h1>
-    ),
-    4: (
-        <h1>4</h1>
-    ),
-}
+import { arrow } from "../assets/icons";
 
-const InfoBox = ({text, link, btnText}) => (
-    <div className='info-box'>
-        {text}
+const HomeInfo = ({ currentStage }) => {
+  if (currentStage === 1)
+    return (
+      <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
+        Hi, I'm
+        <span className='font-semibold mx-2 text-white'>Igor</span>
+        👋
+        <br />
+        A Software Engineer from Serbia 🇷🇸
+      </h1>
+    );
+
+  if (currentStage === 2) {
+    return (
+      <div className='info-box'>
+        <p className='font-medium sm:text-xl text-center'>
+          I've gained a diverse set of skills through <br /> my experiences working with various companies.
+        </p>
+
+        <Link to='/about' className='neo-brutalism-white neo-btn'>
+          Learn more
+          <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+        </Link>
+      </div>
+    );
+  }
+
+  if (currentStage === 3) {
+    return (
+      <div className='info-box'>
+        <p className='font-medium text-center sm:text-xl'>
+          Led multiple projects to success. Interested about?
+        </p>
+
+        <Link to='/projects' className='neo-brutalism-white neo-btn'>
+          Visit my portfolio
+          <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+        </Link>
+      </div>
+    );
+  }
+
+  if (currentStage === 4) {
+    return (
+      <div className='info-box'>
+      <p className='font-medium sm:text-xl text-center'>
+      If you're in need of a project completion or seeking <br /> a developer, I'm just a few keystrokes away.
+      </p>
+
+      <Link to='/contact' className='neo-brutalism-white neo-btn'>
+        Let's talk
+        <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+      </Link>
     </div>
-)
+    );
+  }
 
-const HomeInfo = ({currentStage}) => {
-  return renderContent[currentStage] || null
-     
-  
-}
+  return null;
+};
 
-export default HomeInfo
+export default HomeInfo;
