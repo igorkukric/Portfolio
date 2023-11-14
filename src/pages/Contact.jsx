7dfd8c4be4533137607464c85009a83d6b4930c6
Igor_Kukric <igorkukric@gmail.com>
2023-11-14 15:38:23 +0100

    update Fox UI

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -10,6 +10,7 @@ const Contact = () => {
   const formRef = useRef(null)
   const [form, setForm] = useState({name: '', email: '', message: ''})
   const [isLoading, setIsLoading] = useState(false)
+  const [currentAnimation, setCurrentAnimation] = useState('idle')
 
   const handleChange = (e) => {
     setForm({...form, [e.target.name]: e.target.value})
@@ -18,6 +19,8 @@ const Contact = () => {
   const handleSubmit = (e) => {
     e.preventDefault()
     setIsLoading(true)
+    setCurrentAnimation('hit')
+
     // console.log(VITE_APP_EMAILJS_SERVICE_ID)
     emailjs.send(
       import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
@@ -34,16 +37,22 @@ const Contact = () => {
       setIsLoading(false)
       // TODO: show succes message
       //TODO: HIde an alert
-      setForm({name: '', email: '', message: ''})
+
+      setTimeout(() => {
+        setCurrentAnimation('idle')
+        setForm({name: '', email: '', message: ''})
+      }, [3000])
+
     }).catch((error)=> {
       setIsLoading(false)
+      setCurrentAnimation('idle')
       console.log(error)
       //TODO: Show error message
     })
   }
 
-  const handleFocus = () => {}
-  const handleBlur = () => {}
+  const handleFocus = () => setCurrentAnimation('walk')
+  const handleBlur = () => setCurrentAnimation('idle')
 
 
   return (
@@ -111,13 +120,19 @@ const Contact = () => {
       <div className='lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]'>
         <Canvas
           camera={{
-            position: [0, 0, 5]
+            position: [0, 0, 5],
+            fov: 75,
+            near: 0.1,
+            far: 1000
           }}
         >
+          <directionalLight intensity={2.5} position={[0, 0, 1]} />
+          <ambientLight intensity={0.5}/>
           <Suspense fallback={<Loader />}>
             <Fox 
+              currentAnimation={currentAnimation}
               position={[0.5, 0.35, 0]}
-              rotation={[12, 0, 0]}
+              rotation={[12.6, -0.6, 0]}
               scale={[0.5, 0.5, 0.5]}
             />
           </Suspense>
