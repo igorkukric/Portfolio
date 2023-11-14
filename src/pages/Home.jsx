6eb8b226c99b47b3318d4d60e10d8b620c0bdb6f
Igor_Kukric <igorkukric@gmail.com>
2023-11-14 01:11:49 +0100

    add renden content function

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,16 +6,11 @@ import Island from "../models/island";
 import Sky from "../models/Sky";
 import Bird from "../models/Bird";
 import Plane from "../models/Plane";
-
-{
-  /* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
-  MAIN
-</div> */
-}
+import HomeInfo from "../components/HomeInfo";
 
 const Home = () => {
-  const [isRotating, setIsRotating] = useState(false)
-  const [currentStage, setCurrentStage] = useState(1)
+  const [isRotating, setIsRotating] = useState(false);
+  const [currentStage, setCurrentStage] = useState(1);
 
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
@@ -51,6 +46,9 @@ const Home = () => {
 
   return (
     <section className="w-full h-screen relative">
+      <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+        {currentStage && <HomeInfo  currentStage={currentStage}/>}
+      </div>
       <Canvas
         className={`w-full h-screen bg-transparent" ${
           isRotating ? "cursor-grabbing" : "cursor-grab"
@@ -67,7 +65,7 @@ const Home = () => {
           />
 
           <Bird />
-          <Sky isRotating={isRotating}/>
+          <Sky isRotating={isRotating} />
           <Island
             position={islandPosition}
             scale={islandScale}
