fd79bdd81929a380da8db60af9ceef6fee018f9f
Igor_Kukric <igorkukric@gmail.com>
2023-11-14 15:38:02 +0100

    add light, add functions to fox moving

--- src/models/Fox.jsx
+++ src/models/Fox.jsx
@@ -17,7 +17,12 @@ const Fox = ({currentAnimation, ...props}) => {
   const { actions } = useAnimations(animations, group);
  
   useEffect(() => {
-  
+    console.log(actions)
+    Object.values(actions).forEach((action) => action.stop())
+
+    if(actions[currentAnimation]) {
+        actions[currentAnimation].play()
+    }
   }, [actions, currentAnimation])
   
 
